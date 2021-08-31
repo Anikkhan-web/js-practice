ac984ba684bc22e87107a9d39149a70e073de27b
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-30 23:08:23 -0700

    object in function

--- practice.js
+++ practice.js
@@ -1,31 +1,58 @@
+// const student = {
+//     name:'sakib al hasan',
+//     job:'cricketer'
+// }
+// const  person = new Object();
+// console.log(person);
+// const human = Object.create(student);
+// console.log(human);
+
+// class People {
+
+// constructor(name,age){
+//     this.name = name;
+//     this.age= age;
+
+// }
+
+
+// }
+
+// const peop = new People('manus',12)
+// console.log(peop);
+
+
+// function Manus(name){
+
+//     this.name = name;
+
+// }
+// const man = new Manus('hey');
+// console.log(man);
+
 const student = {
-    name:'sakib al hasan',
-    job:'cricketer'
-}
-const  person = new Object();
-console.log(person);
-const human = Object.create(student);
-console.log(human);
+id: 101,
+name:'kibria',
+major: 'mathematics',
+money:5000,
+subject:['engkish','math','calculus'],
+bestFriend:{
+    name:'kundu',
+    major:'physics'
+},
+takeExam: function(){
+    console.log(this.name,'taking exam')
 
-class People {
-
-constructor(name,age){
-    this.name = name;
-    this.age= age;
-
-}
-
-
-}
-
-const peop = new People('manus',12)
-console.log(peop);
-
-
-function Manus(name){
-
-    this.name = name;
+},
+treatDey: function(expence){
+    this.money = this.money -expence;
+    return this.money
 
 }
-const man = new Manus('hey');
-console.log(man);
+
+
+}
+
+student.takeExam();
+const total = student.treatDey(1000);
+console.log(total) ;
