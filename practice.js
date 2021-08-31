57057a03f9749472df2e3a3b6c8065c16b8f52a4
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-30 23:23:51 -0700

    delete values keys entries freege seal

--- practice.js
+++ practice.js
@@ -30,29 +30,52 @@
 // const man = new Manus('hey');
 // console.log(man);
 
-const student = {
-id: 101,
-name:'kibria',
-major: 'mathematics',
-money:5000,
-subject:['engkish','math','calculus'],
-bestFriend:{
-    name:'kundu',
-    major:'physics'
-},
-takeExam: function(){
-    console.log(this.name,'taking exam')
+// const student = {
+// id: 101,
+// name:'kibria',
+// major: 'mathematics',
+// money:5000,
+// subject:['engkish','math','calculus'],
+// bestFriend:{
+//     name:'kundu',
+//     major:'physics'
+// },
+// takeExam: function(){
+//     console.log(this.name,'taking exam')
 
-},
-treatDey: function(expence){
-    this.money = this.money -expence;
-    return this.money
+// },
+// treatDey: function(expence){
+//     this.money = this.money -expence;
+//     return this.money
 
+// }
+
+
+// }
+
+// student.takeExam();
+// const total = student.treatDey(1000);
+// console.log(total) ;
+
+
+const bottle = {
+
+    color:'yellow',
+    hold:'water',
+    price:50,
+    isCleaned:true
 }
 
+const keys = Object.keys(bottle);
+const values = Object.values(bottle);
+const pairs = Object.entries(bottle);
+Object.seal(bottle)
+delete bottle.isCleaned;
 
-}
 
-student.takeExam();
-const total = student.treatDey(1000);
-console.log(total) ;
+
+console.log(bottle);
+console.log(keys);
+
+console.log(values);
+console.log(pairs);
